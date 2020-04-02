cb42f76691da2e29b1de68871170190f394ec491
Igor, PC <bistrovic.igor@gmail.com>
2020-04-02 18:02:08 +0200

    homepage and routing

---
+++ fininexB/Controllers/fininexBController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace fininexB.Controllers
+{
+    public class fininexBController : Controller
+    {
+        public ActionResult HomePage()
+        {
+            return View();
+        }
+    }
+}
